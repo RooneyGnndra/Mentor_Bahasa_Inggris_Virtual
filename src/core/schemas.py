293c8d131c83c40ff436382c6bc1d2e0217469ce
Rooney PC <rooneyganendra@gmail.com>
2026-08-11 10:08:02 +0700

    config schemas.py

---
+++ src/core/schemas.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel, Field
+from typing import Literal
+
+
+class ListeningExerciseSchema(BaseModel):
+    speaker_one: str = Field(..., description="Nama pembicara pertama, misalnya: 'Joe'")
+    speaker_two: str = Field(..., description="Nama pembicara kedua, misalnya: 'Jane'")
+    script: str = Field(
+        ...,
+        description="Dialog yang dibacakan oleh TTS (Text-to-speech), format: 'Joe: ... \\nJane: ...' bergantian",
+    )
+    questions: list[str] = Field(
+        ..., description="Daftar pertanyaan untuk menguji peserta berdasarkan 'script'"
+    )
+
+
+class EvaluateUserIntentionSchema(BaseModel):
+    skill_types: Literal["reading", "speaking", "writing", "listening"] = Field(
+        ..., description="pilihan salah satu skiil types yang dibutuhkan peserta"
+    )
+
+
+class LearningSkillTypesSchema(BaseModel):  # item latihan yang dilakukan peserta
+    category: str = Field(
+        ...,
+        description="salah satu kategori skill types: reading, speaking, writing dan listening",
+    )
+    title: str = Field(..., description="judul latihan")
+    feedback: str = Field(
+        ..., description="penilaian objektif dengan metode sandwich feedback"
+    )
+    score: int = Field(..., description="nilai kemampuan dalam rentang 1 - 10")
+
+
+class LearningReportSchema(BaseModel):  # laporan belajar
+    start_date: str = Field(..., description="tanggal mulai belajar")
+    end_date: str = Field(..., description="tanggal akhir belajar")
+    username: str = Field(..., description="username dari peserta")
+    global_score: int = Field(..., description="nilai keseluruhan")
+    skill_types: list[LearningSkillTypesSchema]
+    markdown_content: str = Field(
+        ..., description="seluruh isi laporan dalam format markdown"
+    )
